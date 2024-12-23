1cf5e0fc8f9cb5533985c7854f08961e9f74e971
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2024-12-23 06:02:20 -0300

    feat: create middleware

--- src/app/contacts/[contactId]/edit/page.tsx
+++ src/app/contacts/[contactId]/edit/page.tsx
@@ -1,8 +1,6 @@
-import { ContactForm } from '@/components/ContactForm';
 import { db } from '@/lib/db';
-import { ArrowLeftIcon } from 'lucide-react';
-import Link from 'next/link';
 import { redirect } from 'next/navigation';
+import EditContactForm from './editContactForm';
 
 
 interface CreateContactPageProps {
@@ -22,22 +20,5 @@ export default async function CreateContactPage({params}: CreateContactPageProps
    return redirect('/')
   }
 
-  return (
-    <>
-      <header>
-        <Link
-          href="/"
-          className="text-muted-foreground flex items-center gap-1 text-xs mb-2 dark:hover:text-sky-300 hover:text-sky-600"
-        >
-          <ArrowLeftIcon className="size-4" />
-          <span>Voltar para a lista</span>
-        </Link>
-        <h1 className="font-semibold text-3xl tracking-tighter">
-          Editar contato
-        </h1>
-      </header>
-
-      <ContactForm contact={contact} />
-    </>
-  );
+  return <EditContactForm contact={contact} />
 }
